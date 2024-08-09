33d987e9a25164404ee5b3f1f8f9fbcdf522d9f7
psawle <psawle4@gmail.com>
2024-08-09 11:17:58 +0530

    after adding build folder

---
+++ build/precache-manifest.e4fc8109db9caf563322d697863c0950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f89d1e7f30616f036c1",
+    "url": "/static/css/main.b67f4534.chunk.css"
+  },
+  {
+    "revision": "4f89d1e7f30616f036c1",
+    "url": "/static/js/main.48cca9a3.chunk.js"
+  },
+  {
+    "revision": "dafad363111a1abd4dbd",
+    "url": "/static/js/runtime~main.bb47a922.js"
+  },
+  {
+    "revision": "cdea791491a29a85cf23",
+    "url": "/static/js/2.b1776d0f.chunk.js"
+  },
+  {
+    "revision": "a9e0708893377349507b",
+    "url": "/static/js/3.a0d46aea.chunk.js"
+  },
+  {
+    "revision": "fd8751714178768dab51f1ffe40a43fe",
+    "url": "/static/media/img8.fd875171.jpg"
+  },
+  {
+    "revision": "f24d7aa3d63bd61ea01ca92f4f06a6d9",
+    "url": "/index.html"
+  }
+];
